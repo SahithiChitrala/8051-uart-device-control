9b0f13ba1bba9456a8b944a88352c32083732e93
HARI SAHITHI CHITRALA <sahithinarayana17@gmail.com>
2026-02-12 17:33:38 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,186 +1,80 @@
 
 #include<reg51.h>
-#include<stdio.h>
-#include<intrins.h>
 
-#define ACK		1
-#define NO_ACK	0
+void Uart_Init();
+void Uart_Txchar(char);
+char Uart_Rxchar();
+void Uart_SendString(char *);
 
+sbit Lamp =P0^0;
+sbit Fan =P0^1;
 
-unsigned char EData[5];
-unsigned char Data;
-void InitSerial(void);
-void DelayMs(unsigned int);
-
-void WriteI2C(unsigned char);
-unsigned char ReadI2C(bit);
-
-void Start(void);
-void Stop(void);
-void ReadBYTE(unsigned int);
-void WriteBYTE(unsigned int);
-
-sbit SCL  =  P2^0;	// connect to SCL pin (Clock)
-sbit SDA  =  P2^1;	// connect to SDA pin (Data)
-
-
-//---------------------------------------
-// Main program
-//---------------------------------------
-void main(void)
+void Uart_Init()
 {
-	 unsigned int i;
-	InitSerial();  		// Initialize serial port  
-	putchar(0x0C);		// clear hyper terminal
-	DelayMs(5);
-	WriteBYTE(0x0000);
-	WriteI2C('8');		//Write Data's Here
-	WriteI2C('0');
-	WriteI2C('5');
-	WriteI2C('1');
-	Stop();	
-	DelayMs(10);
+	TMOD=0x20;
+	TH1=0xFD;
+	SCON =0x50;
+	TR1=1;
+}
+void Uart_Txchar(char Data)
+{
+	SBUF=Data;
+	while(TI==0);
+	TI=0;
+}
+char Uart_Rxchar()
+{
+	char dat;
+	while(RI==0);
+	RI=0;
+	dat=SBUF;
+	return(dat);
+	
+}
 
-	ReadBYTE(0x0000); 
-	EData[0] = ReadI2C(NO_ACK);	
-	EData[1] = ReadI2C(NO_ACK);	
-	EData[2] = ReadI2C(NO_ACK);	
-	EData[3] = ReadI2C(NO_ACK);	
+void Uart_SendString(char *str)
+{
+	int i;
+	for (i=0;str[i]!=0;i++)
+	{
+		Uart_Txchar(str[i]);
+	}
+}
+
+void main()
+{
+	char Data_in;
+	Uart_Init();
+	P0=0;
+	while(1)
+	{
+		Data_in=Uart_Rxchar();
+		if(Data_in =='1')
+		{
+			Lamp=1;
+			Uart_SendString("Lamp On\r\n");
+		}
+		else if(Data_in =='2')
+		{
+			Lamp=0;
+			Uart_SendString("Lamp Off\r\n");
+		}
+		else if(Data_in =='3')
+		{
+			Fan=1;
+			Uart_SendString("Fan On\r\n");
 			
+		}
+		else if(Data_in =='4')
+		{
+			Fan=0;
+			Uart_SendString("Fan Off\r\n");
+		}
+		else
+		{
+			Uart_SendString("choose 1,2,3,4\r\n");
+		}
+		
+	}
 	
-	for(i=0;i<4;i++)
-	{
-	printf("%c",EData[i]);	// display data    	  */
-	DelayMs(100);
-	}
-while(1);
 }
-
-//---------------------------------------
-// Initialize serial port
-//---------------------------------------
-void InitSerial(void)
-{
-  	SCON = 0x52;    // setup serial port control 
-  	TMOD = 0x20;    // hardware (9600 BAUD @11.0592MHZ) 
-  	TH1  = 0xFD;    // TH1
-	TR1	 = 1;  		// Timer 1 on
-}
-
-
-//-------------------------------
-// start I2C
-//-------------------------------
-void Start(void)
-{
-    SDA = 1;
-	SCL = 1;
-	_nop_();_nop_();
-	SDA = 0;
-	_nop_();_nop_();
-	SCL = 0;
-	_nop_();_nop_();
-}
-
-//-------------------------------
-// stop I2C
-//-------------------------------
-void Stop(void)
-{
-	SDA = 0;	    	
-	_nop_();_nop_();
-	SCL = 1;
-	_nop_();_nop_();
-	SDA = 1;	
-}
-
-//-------------------------------
-// Write I2C
-//-------------------------------
-void WriteI2C(unsigned char Data)
-{    
- unsigned int i;
-	for (i=0;i<8;i++)
-	{
-        SDA = (Data & 0x80);
-		SCL=1;SCL=0;
-		Data<<=1;
-	}
-
-  	SCL = 1; 
-	_nop_();_nop_();
-	SCL = 0;
-
-}
-
-//-------------------------------
-// Read I2C
-//-------------------------------
-unsigned char ReadI2C(bit ACK_Bit)
-{
-	 unsigned int i;
-	
-    Start();
-	WriteI2C(0xA1);
-    
-
-    SDA = 1;	
-	for (i=0;i<8;i++)
-	{
-		SCL   = 1;		
-		Data<<= 1;
-		Data  = (Data | SDA);		
-		SCL   = 0;
-		_nop_();
-	}
-    
- 	if (ACK_Bit == 1)
-	SDA = 0; // Send ACK		
-	else		
-	SDA = 1; // Send NO ACK				
-
-	_nop_();_nop_();
-	SCL = 1;		
-	_nop_();_nop_();
-	SCL = 0;
-	Stop();
-	return Data;
-}
-
-//-------------------------------
-// Read 1 byte form I2C
-//-------------------------------
-void ReadBYTE(unsigned int Addr)
-{
-	Start();
-	WriteI2C(0xA0);
-	WriteI2C((unsigned char)(Addr>>8)&0xFF);
-	WriteI2C((unsigned char)Addr&0xFF);
-}
-
-//-------------------------------
-// Write 1 byte to I2C
-//-------------------------------
-void WriteBYTE(unsigned int Addr)
-{
-	Start();
-	WriteI2C(0xA0);
-	WriteI2C((unsigned char)(Addr>>8)&0xFF);	// send address high
-	WriteI2C((unsigned char)Addr&0xFF);			// send address low	
-}
-
-
-//---------------------------------------
-// Delay mS function
-//---------------------------------------
-void DelayMs(unsigned int count) 
-{  // mSec Delay 11.0592 Mhz 
-    unsigned int i;		       		// Keil v7.5a 
-    while(count) 
-	{
-        i = 110; 
-		while(i>0) i--;
-        count--;
-    }
-}
-
